c21499012f3260a7cb3f347ef8971615e16cf00f
billy1380 <william.shakour@gmail.com>
2014-06-30 19:25:14 +0100

    added done file helper

---
+++ src/io/reflection/pullmodel/DoneHelper.java
@@ -0,0 +1,58 @@
+//
+//  DoneHelper.java
+//  pull.model
+//
+//  Created by (William Shakour (billy1380)) on 30 Jun 2014.
+//  Copyright © 2014 Reflection.io Ltd. All rights reserved.
+//
+package io.reflection.pullmodel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author William Shakour (billy1380)
+ * 
+ */
+public class DoneHelper {
+	public static void writeDoneFile(String filePath) throws IOException {
+		String doneFilePath = filePath + ".done";
+
+		FileOutputStream out = null;
+
+		try {
+			out = new FileOutputStream(doneFilePath);
+			out.write(SimpleDateFormat.getInstance().format(new Date()).getBytes());
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+
+	public static Date doneAt(String filePath) throws IOException, ParseException {
+		String doneFilePath = filePath + ".done";
+		Date date = null;
+		FileInputStream in = null;
+
+		try {
+			in = new FileInputStream(doneFilePath);
+			byte[] buffer = new byte[1024];
+			int read = 0;
+			if ((read = in.read(buffer)) > 0) {
+				String content = new String(buffer, 0, read);
+				date = SimpleDateFormat.getInstance().parse(content);
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+
+		return date;
+	}
+}
